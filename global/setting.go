3a93c47bd5ecc634669b1984933292c19ce1db34
ricky <rickyfor@qq.com>
2024-08-11 18:02:03 +0800

    init

---
+++ global/setting.go
@@ -0,0 +1,15 @@
+package global
+
+import (
+	"go-web-pkg/logger"
+	"go-web-pkg/setting"
+)
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	EmailSetting    *setting.EmailSettingS
+	JWTSetting      *setting.JWTSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+	Logger          *logger.Logger
+)
